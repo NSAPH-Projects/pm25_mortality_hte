d53ce90073a7c0816c9e37fc50b3e1a0905855fd
Lauren Mock <laurenmock17@gmail.com>
2024-02-26 11:57:21 -0500

    only 2 interactions for now

--- analysis/5_Outcome_Model.R
+++ analysis/5_Outcome_Model.R
@@ -12,21 +12,20 @@ library(tidyverse)
 
 # # Load data
 load("data/intermediate/rolling_cohort.RData")
+
 # # Work with a random sample for now
 # set.seed(17)
 # keep_idx <- sample(unique(dt[,qid]), 1000, replace = FALSE)
 # dt <- dt[qid %in% keep_idx,]
 # save(dt, file = "data/intermediate/rolling_cohort_1000.RData")
 
-#load("data/intermediate/rolling_cohort_1000.RData")
+# load("data/intermediate/rolling_cohort_1000.RData")
 
 
 
 #########################
 
 # time discrete logistic regression model
-
-# then also make all of these interactions with PM2.5 and do all the zip-level vars (no interaction)
 
 outcome_fit <- glm(dead_lead ~ 
                      
@@ -45,22 +44,20 @@ outcome_fit <- glm(dead_lead ~
                      summer_tmmx + summer_rmax + winter_tmmx + winter_rmax +
                      
                      # some one-way interactions
-                     #pm25:sex + 
                      pm25:age + 
-                     #pm25:as.factor(race) + 
                      pm25:dual, 
                    
                    family = binomial(), 
                    data = dt)
-
-# save(outcome_fit, file = "results/outcome_model.RData")
-# 
-load("results/outcome_model.RData")
+#save(outcome_fit, file = "results/outcome_model.RData")
+#load("results/outcome_model.RData")
 
 summary(outcome_fit)
 
 
-# now make a quick little visualization to look at interaction terms
+# now make a quick visualization to look at interaction terms
+
+
 
 
 
