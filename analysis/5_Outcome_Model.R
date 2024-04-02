0666d312bce56b8b7d86e902f0787df8f96c3dd9
Lauren Mock <laurenmock17@gmail.com>
2024-04-02 12:12:10 -0400

    fit with interactions selected by LASSO

--- analysis/5_Outcome_Model.R
+++ analysis/5_Outcome_Model.R
@@ -24,35 +24,83 @@ load("data/intermediate/rolling_cohort.RData")
 
 # time discrete logistic regression model
 
+# outcome_fit <- glm(dead_lead ~ 
+#                      
+#                      # exposure
+#                      pm25 +
+#                      
+#                      # calendar year and year of follow-up
+#                      as.factor(year) + as.factor(year_follow) + 
+#                      
+#                      # # census division
+#                      as.factor(census_div) + 
+#                      
+#                      # sex, age, race, Medicaid eligibility
+#                      sex + age + as.factor(race) + dual +
+#                      
+#                      # chronic conditions
+#                      hypert_ever + ischmcht_ever + diabetes_ever + chrnkidn_ever +
+#                      
+#                      # area-level covariates
+#                      poverty + popdensity + medianhousevalue + medhouseholdincome + 
+#                      pct_owner_occ + education + smoke_rate + mean_bmi +
+#                      summer_tmmx + summer_rmax + winter_tmmx + winter_rmax +
+#                      
+#                      # all individual-level one-way interactions
+#                      pm25:age + pm25:sex + pm25:dual + pm25:as.factor(race) +
+#                      pm25:hypert_ever + pm25:ischmcht_ever + 
+#                      pm25:diabetes_ever + pm25:chrnkidn_ever, 
+#                    
+#                    family = binomial(), 
+#                    data = dt)
+# 
+# save(outcome_fit, file = "results/outcome_model/model_all.RData")
+# load("results/outcome_model/model_lasso_vars.RData")
+
+
 outcome_fit <- glm(dead_lead ~ 
                      
                      # exposure
                      pm25 +
                      
-                     # calendar year and year of follow-up
-                     as.factor(year) + as.factor(year_follow) + 
+                     # # calendar year and year of follow-up
+                     # as.factor(year) + as.factor(year_follow) + 
                      
-                     # census division
-                     as.factor(census_div) + 
+                     # # # census division
+                     # as.factor(census_div) + 
                      
                      # sex, age, race, Medicaid eligibility
-                     sex + age + as.factor(race) + dual +
+                     sex + age + 
+                     #as.factor(race) + 
+                     asian + hispanic + 
+                     dual +
                      
-                     # area-level covariates
-                     poverty + popdensity + medianhousevalue + medhouseholdincome + 
-                     pct_owner_occ + education + smoke_rate + mean_bmi +
-                     summer_tmmx + summer_rmax + winter_tmmx + winter_rmax +
+                     # chronic conditions
+                     hypert_ever + ischmcht_ever + diabetes_ever + chrnkidn_ever +
                      
-                     # some one-way interactions
-                     pm25:age + 
-                     pm25:dual +
-                     pm25:hypert_ever, 
+                     # # area-level covariates
+                     # poverty + popdensity + medianhousevalue + medhouseholdincome + 
+                     # pct_owner_occ + education + smoke_rate + mean_bmi +
+                     # summer_tmmx + summer_rmax + winter_tmmx + winter_rmax +
+                     
+                     # all individual-level one-way interactions
+                     #pm25:age + 
+                     pm25:sex + 
+                     #pm25:dual + 
+                     #pm25:as.factor(race) +
+                     pm25:asian +
+                     pm25:hispanic + 
+                     pm25:hypert_ever + pm25:ischmcht_ever + 
+                     pm25:diabetes_ever + pm25:chrnkidn_ever, 
                    
                    family = binomial(), 
                    data = dt)
 
-#save(outcome_fit, file = "results/outcome_model.RData")
-load("results/outcome_model.RData")
+
+#save(outcome_fit, file = "results/outcome_model/model_lasso_vars.RData")
+load("results/outcome_model/model_lasso_vars.RData")
+
+
 
 summary(outcome_fit)
 
