7d9ce29cddb4b69903f8961008d183d3bc599117
Lauren Mock <laurenmock17@gmail.com>
2024-09-20 12:05:23 -0400

    get adjusted p-vals

--- analysis/subpops_figure.R
+++ analysis/subpops_figure.R
@@ -7,10 +7,13 @@ library(ggplot2)
 #----- load model results
 
 # path to results
-path_res <- "results/subpop/"
+path_res <- "results/subpop/unstratified/"
 
 # get all file names in directory
 file_names <- list.files(path_res)
+
+# restrict to the ones that aren't stratified
+file_names <- file_names[!str_detect(file_names, "strat")]
 
 # load into a list
 coeff <- lapply(paste0(path_res, file_names), readRDS)
@@ -31,9 +34,12 @@ get_pm25_effect <- function(x){
   pm25_OR_low <- (pm25_row["Estimate"] - 1.96 * pm25_row["Std. Error"]) |> exp()
   pm25_OR_high <- (pm25_row["Estimate"] + 1.96 * pm25_row["Std. Error"]) |> exp()
   
-  names(pm25_OR) <- names(pm25_OR_low) <- names(pm25_OR_high) <- NULL
+  # get p-value
+  pm25_pval <- pm25_row["Pr(>|z|)"]
+  
+  names(pm25_OR) <- names(pm25_OR_low) <- names(pm25_OR_high) <- names(pm25_pval) <- NULL
 
-  return(c(OR = pm25_OR, low = pm25_OR_low, high = pm25_OR_high))
+  return(c(OR = pm25_OR, low = pm25_OR_low, high = pm25_OR_high, pval = pm25_pval))
 }
 
 # get all results in df
@@ -41,6 +47,16 @@ pm25_df <- sapply(coeff, get_pm25_effect) |>
   t() |> 
   as.data.frame()
 
+# adjust p-values with Benjamini-Hochberg
+pm25_df <- pm25_df %>%
+  mutate(#pval_adj = p.adjust(pval, method = "BH")
+         pval_adj = p.adjust(pval, method = "BY"))
+
+# stars for significance
+pm25_df <- pm25_df %>% 
+  mutate(sig = ifelse(pval_adj < 0.001, "***", 
+                      ifelse(pval_adj < 0.01 & pval_adj >= 0.001, "**", 
+                             ifelse(pval_adj < 0.05 & pval_adj >= 0.01, "*", ""))))
 
 #---- prepare data for plotting
 
@@ -80,25 +96,35 @@ pm25_df <- left_join(pm25_df, prev_df, by = "cond_abbr")
 
 # new column with condition name and prevalence
 pm25_df <- pm25_df |>
-  mutate(cond_name_prev = paste0(cond_name, " (", round(prevalence, 1), "%)"))
+  mutate(cond_name_prev = paste0(cond_name, " (", 
+                                 formatC(round(prevalence, 1), format = "f", digits = 1), 
+                                 "%)"))
 
 # sort by OR
 pm25_df$cond_name_prev <- factor(pm25_df$cond_name_prev,
                             levels = pm25_df$cond_name_prev[order(pm25_df$OR)])
 
+# save plotting levels (use same order for stratified models)
+cond_abbr_order <- pm25_df$cond_abbr[order(pm25_df$OR)]
+saveRDS(cond_abbr_order, "data/intermediate/cond_abbr_OR_order.rds")
+
+
 #----- plot
 
+pdf("results/figures/subpop_effects.pdf", width = 7, height = 6)
 pm25_df |>
   mutate(special = ifelse(cond_abbr %in% c("nohosp", "fullpop"), TRUE, FALSE)) |>
   ggplot(aes(x = OR, y = cond_name_prev, col = special)) +
   geom_point() +
-  geom_errorbar(aes(xmin = low, xmax = high), width = 0.3) +
+  geom_errorbar(aes(xmin = low, xmax = high), width = 0) +
   geom_vline(xintercept = 1, lty = 2, col = "gray50") +
-  labs(x = expression("OR for mortality with 1 \u03bcg/" * m^3 * " increase in " * PM[2.5]), 
+  labs(x = expression("OR for mortality with 1 " * mu * "g/" * m^3 * " increase in " * PM[2.5]), 
        y = "") +
-  scale_color_manual(values = c(`TRUE` = "orange2", `FALSE` = "black")) +
-  theme_minimal() +
-  theme(legend.position = "none")
-
-
-
+  scale_color_manual(values = c(`TRUE` = "salmon", `FALSE` = "black")) +
+  geom_text(aes(x = 0.999, y = cond_name_prev, label = sig)) +
+  theme_classic(base_size = 12) +
+  theme(legend.position = "none",
+        axis.line.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        panel.grid.major.y = element_line())
+dev.off()
