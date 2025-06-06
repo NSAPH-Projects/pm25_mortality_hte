cf56d68eebb9534208393d7eacb0a2828bed6878
Lauren Mock <laurenmock17@gmail.com>
2025-06-06 16:34:54 -0400

    prevalence, not % obs

--- analysis/3_calculate_prevalence.R
+++ analysis/3_calculate_prevalence.R
@@ -13,7 +13,7 @@ library(dplyr)
 
 # Load data
 dt <- readRDS("data/intermediate/rolling_cohort.rds")
-# dt <- ("data/intermediate/rolling_cohort_1000.rds")
+# dt <- readRDS("data/intermediate/rolling_cohort_10000.rds")
 
 
 #---- prepare data
@@ -21,30 +21,26 @@ dt <- readRDS("data/intermediate/rolling_cohort.rds")
 # CCW indicator columns
 ccw_names <- names(dt)[str_detect(names(dt), "ever")]
 
-# new column that = 1 for individuals with no previous hosp (all other indicators = 0)
-dt[, nohosp := as.integer(rowSums(.SD) == 0), .SDcols = ccw_names]
+# # new column that = 1 for individuals with no previous hosp (all other indicators = 0)
+# dt[, nohosp := as.integer(rowSums(.SD) == 0), .SDcols = ccw_names]
 
 # new column that = 1 for all individuals
 dt[, fullpop := 1]
 
 # List of binary indicator column names
-subpops <- c(ccw_names, "nohosp", "fullpop")
-
+#subpops <- c(ccw_names, "nohosp", "fullpop")
+subpops <- c(ccw_names, "fullpop")
 
 #---- calculate prevalence
 
-# all rows
-prev_dt <- dt[, lapply(.SD, function(x) sum(x) / .N * 100),
+# what % of people in the cohort contributed to each subgroup?
+
+prev_dt <- dt[year == last_year_ffs - 1, lapply(.SD, function(x) sum(x) / .N * 100),
               .SDcols = subpops] |>
   gather(key = "cond_abbr", value = "prevalence") |>
   mutate(cond_abbr = str_remove(cond_abbr, "_ever"))
 
-# # 2010 only
-# prev_dt <- dt[year == 2010, lapply(.SD, function(x) sum(x) / .N * 100),
-#                    .SDcols = subpops] |>
-#   gather(key = "cond_abbr", value = "prevalence") |>
-#   mutate(cond_abbr = str_remove(cond_abbr, "_ever"))
 
 saveRDS(prev_dt, 
-        file = paste0("results/subpop_prevalence.rds"))
+        file = paste0("results/subgroup_prevalence.rds"))
 
